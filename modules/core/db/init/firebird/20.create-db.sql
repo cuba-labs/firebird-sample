703054768acb199ab42917b342e2ad8c3611dd21
budarov <budarov@haulmont.com>
2020-09-17 11:26:32 +0400

    delete sample entities and their screens & db scripts (they are a hassle when using this project as app component)

--- modules/core/db/init/firebird/20.create-db.sql
+++
@@ -1,4 +0,0 @@
--- begin FB_STUDENT
-alter table FB_STUDENT add constraint FK_FB_STUDENT_ON_GROUP foreign key (GROUP_ID) references FB_GROUP(ID)^
-create index IDX_FB_STUDENT_ON_GROUP on FB_STUDENT (GROUP_ID)^
--- end FB_STUDENT
